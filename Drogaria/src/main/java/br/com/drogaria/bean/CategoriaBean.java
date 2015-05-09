e50d50424299df44110a509abfbb6c5c4a46755d
mcgacarol <mcgacarol@gmail.com>
2015-05-09 14:56:48 -0300

    Comits da implementação das telas

--- Drogaria/src/main/java/br/com/drogaria/bean/CategoriaBean.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/CategoriaBean.java
@@ -1,10 +1,57 @@
 package br.com.drogaria.bean;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogaria.dao.CategoriaDAO;
+import br.com.drogaria.domain.Categoria;
 
 @ManagedBean
 @ViewScoped
 public class CategoriaBean {
-
+	
+	private Categoria categoria;
+	private List<Categoria> categorias;
+	
+	public void salvar(){
+		CategoriaDAO categoriaDAO = new CategoriaDAO();
+		categoriaDAO.salvar(categoria);
+		Messages.addGlobalInfo("Categoria salva com sucesso");
+		categoria = new Categoria();
+		
+		listar();
+	}
+	
+	public void listar(){
+		CategoriaDAO categoriaDAO = new CategoriaDAO();
+		categorias = categoriaDAO.listar();
+	}
+	
+	
+	@PostConstruct
+	public void iniciar(){
+		categoria = new Categoria();
+		listar();
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+	
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
 }
--- Drogaria/src/test/java/br/com/drogaria/util/HibernateUtilTest.java
+++ Drogaria/src/test/java/br/com/drogaria/util/HibernateUtilTest.java
@@ -2,14 +2,12 @@ package br.com.drogaria.util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class HibernateUtilTest {
 	// Anotação que informa que o método será um caso de teste
 	// Anotação que informa que o método será ignorado durante os testes
 	@Test
-	@Ignore
 	public void conectar(){
 		// Captura a fábrica de sessão
 		SessionFactory fabrica = HibernateUtil.getSessionFactory();
